3f6c27ef8ee97834626d77cde4daf6aa95a625d7
mucahitgoktas <mucahitgoktas@gmail.com>
2021-06-04 19:45:30 +0300

    Başlangıç Şablonlarının Oluşturulması

---
+++ DevFramework.Northwind.Business/Concrete/Managers/ProductManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using DevFramework.Core.CrossCuttingConcerns.Validation.FluentValidation;
+using DevFramework.Northwind.Business.Abstract;
+using DevFramework.Northwind.Business.ValidationRules.FluentValidation;
+using DevFramework.Northwind.DataAccess.Abstract;
+using DevFramework.Northwind.Entities.Concrete;
+using DevFramework.Core.Aspects.Postsharp;
+using DevFramework.Core.Aspects.Postsharp.CacheAspects;
+using DevFramework.Core.Aspects.Postsharp.TransactionAspects;
+using DevFramework.Core.Aspects.Postsharp.ValidationAspects;
+using DevFramework.Core.CrossCuttingConcerns.Caching.Microsoft;
+using DevFramework.Core.DataAccess;
+
+namespace DevFramework.Northwind.Business.Concrete.Managers
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        [CacheAspect(typeof(MemoryCacheManager))]
+        public List<Product> GetAll()
+        {
+            return _productDal.GetList();
+
+        }
+
+        public Product GetById(int id)
+        {
+            return _productDal.Get(p => p.ProductId == id);
+        }
+
+        [FluentValidationAspect(typeof(ProductValidatior))]
+        [CacheRemoveAspect(typeof(MemoryCacheManager))]
+        public Product Add(Product product)
+        {
+            return _productDal.Add(product);
+        }
+
+        [FluentValidationAspect(typeof(ProductValidatior))]
+        public Product Update(Product product)
+        {
+            return _productDal.Update(product);
+        }
+
+        [TransactionScopeAspect]
+        public void TransactionalOperation(Product product1, Product product2)
+        {
+            _productDal.Add(product1);
+            // Business Codes
+            _productDal.Update(product2);
+        }
+    }
+}
